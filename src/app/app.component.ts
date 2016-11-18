ae79450f61d5d9e2327c61e18a81793a53a537ce
Burhan Mubarok <burhanmubarok@gmail.com>
2016-11-18 10:06:22 +0700

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  message = '';
+
+  onClickMe() {
+    if (this.message == '') {
+      this.message = 'You are a good coder';
+    }
+    else{
+      this.message = 'sure, you are a good coder';
+    }
+  }
+
+  values = '';
+  onKey(event: any){
+    this.values += event.target.value + ' | '; 
+  }
+
+  titleValue = '';
+  onKeyUp(val: string){
+    this.titleValue = val;
+  }
+
+  heroes = ['Saras 008', 'Panji Manusia Milenium', 'Pitung', 'Kian santang', 'Maja Pahit'];
+
+  addHero(newHero: string){
+    if(newHero){
+      this.heroes.push(newHero);
+    }
+  }
 }
